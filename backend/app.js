447874b43a63d08cea5a85b16f36ef15515ff53e
Suvin S <your-email@example.com>
2026-07-25 09:06:00 +0530

    Fix CORS

--- backend/app.js
+++ backend/app.js
@@ -24,12 +24,13 @@ app.use(express.urlencoded({ extended: true }));
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://skillmart.vercel.app",
+  "https://skill-mart-one.vercel.app",
 ];
+
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
